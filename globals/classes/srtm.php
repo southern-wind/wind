57d820d14804002410fe69a3d4d0503310ed263e
spin <spin@ctwug.za.net>
2015-12-14 23:01:28 +0200

    Update globals/classes/srtm.php.bak to Southern Hemisphere compatible.

--- globals/classes/srtm.php
+++ globals/classes/srtm.php
@@ -20,94 +20,92 @@
 /**
  * Class to handle SRTM Data
  */
+
 class srtm {
-	
-	var $data_path;
-	
-	function __construct($data_path='') {
-		$this->data_path = $data_path;
-	}
-	
+
+        var $data_path;
+
+function __construct($data_path='') {
+                $this->data_path = $data_path;
+        }
+
         public static function get_filename($lat, $lon) {
                 $ll = srtm::get_lat_long_adjustments($lat, $lon);
                 //print_R($ll);
-                //always return filename with positive numbers (eg S-33 -> S33)
-                return $ll['lat_dir'] . sprintf("%02.0f", abs((integer)($lat+$ll['lat_adj'])))
-                  .$ll['lon_dir'].sprintf("%03.0f", abs((integer)($lon+$ll['lon_adj']))).'.hgt';
+                $n_lat = ($lat+$ll['lat_adj']);
+                $n_lon = ($lon+$ll['lon_adj']);
+                return $ll['lat_dir'].sprintf("%02.0f",abs((integer)($lat+$ll['lat_adj'])))
+                       .$ll['lon_dir'].sprintf("%03.0f",abs((integer)($lon+$ll['lon_adj']))).'.hgt';
         }
 
         function get_lat_long_adjustments($lat,$lon) {
-          if ($lat < 0) {
-            $r['lat_dir'] = 'S';
-            $r['lat_adj'] = 1;
-          } else {
-            $r['lat_dir'] = 'N';
-            $r['lat_adj'] = 0;
-          }
-          if ($lon < 0) {
-            $r['lon_dir'] = 'W';
-            $r['lon_adj'] = 1;
-          } else {
-            $r['lon_dir'] = 'E';
-            if ($r['lat_dir'] == 'S') {
-              $r['lon_adj'] = -0.75;;
-            } else {
-              $r['lon_adj'] = 0;
-            }
-          }
-          return $r;
+                //adjustment for filenames.
+                // as the filename is based upon the bottom left corner
+                // S locations will be records for -31.0 to -31.9 in a S32 file.
+                if ($lat < 0) {
+                        $r['lat_dir'] = 'S';
+                        $r['lat_adj'] = -1;
+                } else {
+                        $r['lat_dir'] = 'N';
+                        $r['lat_adj'] = 0;
+                }
+                if ($lon < 0) {
+                        $r['lon_dir'] = 'W';
+                        $r['lon_adj'] = 0;
+                } else {
+                        $r['lon_dir'] = 'E';
+                        $r['lon_adj'] = 0;
+                }
+                return $r;
         }
 
         function get_elevation($lat, $lon, $round=TRUE) {
 
-		$filename = $this->data_path.$this->get_filename($lat,$lon);
+                $filename = $this->data_path.$this->get_filename($lat,$lon);
+                if ($lat === '' || $lon === '' || !file_exists($filename)) return FALSE;
 
-		if ($lat === '' || $lon === '' || !file_exists($filename)) return FALSE;
+                $file = fopen($filename, 'r');
 
-		$file = fopen($filename, 'r');
+                $ll = $this->get_lat_long_adjustments($lat,$lon);
+                $lat_dir = $ll['lat_dir'];
+                $lat_adj = $ll['lat_adj'];
+                $lon_dir = $ll['lon_dir'];
+                $lon_adj = $ll['lon_adj'];
+                $y = $lat;
+                $x = $lon;
 
-		$ll = $this->get_lat_long_adjustments($lat,$lon);
-		$lat_dir = $ll['lat_dir'];
-		$lat_adj = $ll['lat_adj'];
-		$lon_dir = $ll['lon_dir'];
-		$lon_adj = $ll['lon_adj'];
-		$y = $lat;
-		$x = $lon;
+                //whole number - full number * lines (1201)
+                // * (
+                $line = (integer)(( (integer)$lat - $lat )* 1201);
+                $pixel = round(($lon - (integer)$lon ) * 1201);
 
-		$offset = ( (integer)(($x - (integer)$x + $lon_adj) * 1200)
-			* 2 + (1200 - (integer)(($y - (integer)$y + $lat_adj) * 1200)) 
-			* 2402 );
-		fseek($file, $offset);
-		$h1 = bytes2int(strrev(fread($file, 2)));
-		$h2 = bytes2int(strrev(fread($file, 2)));
-		fseek($file, $offset-2402);
-		$h3 = bytes2int(strrev(fread($file, 2)));
-		$h4 = bytes2int(strrev(fread($file, 2)));
-		fclose($file);
-	
-		$m = max($h1, $h2, $h3, $h4);
-		for($i=1;$i<=4;$i++) {
-			$c = 'h'.$i;
-			if ($$c == -32768)
-				$$c = $m;
-		}
-	
-		$fx = ($lon - ((integer)($lon * 1200) / 1200)) * 1200;
-		$fy = ($lat - ((integer)($lat * 1200) / 1200)) * 1200;
-	
-		// normalizing data
-		$elevation = ($h1 * (1 - $fx) + $h2 * $fx) * (1 - $fy) + ($h3 * (1 - $fx) + $h4 * $fx) * $fy;
-		if ($round)
-			$elevation = round($elevation);
-		return $elevation;
-	}
+                //Debug
+                if (isset($_SESSION['userdata']['id']) && $_SESSION['userdata']['id']=="-1") {
+                        //echo "filename:$filename, lat:$lat, lon:$lon\n";
+                        //echo "Line:$line, pixel:$pixel, iLine:$iLine,
+			//offset $offset_no_adj <br />\n";
+                }
+                //file naming is from lower left corner.
+                //data is stored within the file from the upper left corner.
+                $offset = (($line*1201) + $pixel)*2;
+
+                fseek($file, $offset);
+                //reverse string - file read (handle, length)
+                $h1 = bytes2int(strrev(fread($file, 2)));
+                if ($h1 == -32768) {
+                        $h1 = 0;
+                }
+                fclose ($file);
+                return $h1;
+        }
 
 }
 
 function bytes2int($val) {
-	$t = unpack("s", $val);
-	$ret = $t[1];
-	return $ret;
+        $t = unpack("s", $val);
+        $ret = $t[1];
+        return $ret;
 }
+
 
 ?>

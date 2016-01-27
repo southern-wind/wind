108b19ee90d7ef6167cf35ff10b4cc89090d52de
jammin84 <jammin84@users.noreply.github.com>
2016-01-27 09:53:40 +0800

    Create schema-v1.3.inc.php

---
+++ tools/updates/schema-v1.3.inc.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * WiND - Wireless Nodes Database
+*
+* Copyright (C) 2005-2014 	by WiND Contributors (see AUTHORS.txt)
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+$update = new DBUpdateDescriptor(new SchemaVersion(1,2), new SchemaVersion(1,3));
+/******************************************
+ *         ip_addresses database changes          */
+
+// column for zone_types
+$update->newColumn('ip_addresses', 'zone_type', 'enum('forward', 'reverse', 'cname')', array(
+                'default' => 'forward'));
+
+return $update;
